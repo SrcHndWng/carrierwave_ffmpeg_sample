236c4e828feff24a49bb13e35e95f3f4cd754ef9
SrcHndWng <undersourcecode@hotmail.co.jp>
2014-05-01 21:22:04 +0900

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarrierwaveFfmpegSample::Application.config.secret_key_base = '2e7dd0a3dbf2f3cf59bfc95086b6582b2607b32cd05306de4dec4381c4c9f1e8bca087dab81f121707fcc034ff4b3de5cc8d3d146fe380b8640c332c76e1a370'
